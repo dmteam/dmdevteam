cf9214c8b607f9b499a76de1716e87ea0ff318ca
dmteam <info@ditomismo.com>
2013-11-16 21:42:39 +0800

    Add login.php.

---
+++ login.php
@@ -0,0 +1,14 @@
+<form id="form" name="form" method="post" action="allocate.php">
+<h1>Sign-up form</h1>
+<br>
+Email:
+<br>
+<input type="text" name="username"  />
+<br>
+Password:<br>
+<input type="pass" name="password"  />
+<br>
+
+<button type="submit" name = "b1">Login</button><br>
+
+</form>
